27106825c4be0fb87e69f0f729d369e166e6550b
Panchanathang <157068157+Panchanathang@users.noreply.github.com>
2024-07-10 18:38:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from meta_ai_api import MetaAI
-import os 
+#import os 
 
 app = Flask(__name__)
 CORS(app) # <-- Enable CORS for all routes
@@ -10,7 +10,7 @@ ai = MetaAI()
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'Welcome to my chatbot API!'
+    return app.send_static_file('index.html')
 
 @app.route('/endpoint', methods=['POST'])
 def handle_chatbot_request():
@@ -20,7 +20,7 @@ def handle_chatbot_request():
 
 if __name__ == '__main__':
      app.run(debug=True)
-
+app.run(debug=True)
 #if __name__ == '__main__':
  #    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
  #app.listen(process.env.PORT || 5000)
