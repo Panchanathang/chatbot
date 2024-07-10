a0db724db28a3a7e31a2f74f44a332fb74c1e995
Panchanathang <157068157+Panchanathang@users.noreply.github.com>
2024-07-10 17:25:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from meta_ai_api import MetaAI
-import os 
+#import os 
 
 app = Flask(__name__)
 CORS(app) # <-- Enable CORS for all routes
