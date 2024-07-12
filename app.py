aa57ab2aa0fb1878571d950bdeb6d95bb00a9b29
Panchanathang <157068157+Panchanathang@users.noreply.github.com>
2024-07-12 10:04:16 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://chatbot-lsm1.onrender.com"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 ai = MetaAI()
