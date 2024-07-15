8b5bb9c4674ea102175dea8b4a6969693aac40d3
Panchanathang <157068157+Panchanathang@users.noreply.github.com>
2024-07-15 09:11:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 from meta_ai_api import MetaAI
 from flask_cors import CORS
-
+import logging
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
-
 ai = MetaAI()
 
+# Setup logging
+logging.basicConfig(level=logging.DEBUG)
 
 # Serve the HTML file
 @app.route('/')
@@ -19,11 +20,14 @@ def home():
 @app.route('/endpoint', methods=['POST'])
 def handle_chatbot_request():
     message = request.json['message']
-    response = ai.prompt(message)
-    return jsonify({'response': response})
-
-#if __name__ == '__main__':
- #    app.run(debug=True)
+    logging.debug(f"Received message: {message}")
+    try:
+        response = ai.prompt(message)
+        logging.debug(f"Meta AI response: {response}")
+        return jsonify({'response': response})
+    except Exception as e:
+        logging.error(f"Error handling request: {e}")
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=10000)
