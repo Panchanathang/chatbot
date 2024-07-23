3084571dc4da72bf07509f109ffd6a5bb98c68e4
Panchanathang <157068157+Panchanathang@users.noreply.github.com>
2024-07-23 17:42:56 -0400

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,9 @@ def handle_chatbot_request():
 
 
 if __name__ == '__main__':
- #    app.run(debug=True)
-
+#    app.run(debug=True)
 #if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+#  app.run(debug=True, host='0.0.0.0', port=5000)
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
