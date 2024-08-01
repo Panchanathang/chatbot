798bdf02e1ac7fbe59bba9772b7971fef418c37b
Panchanathang <157068157+Panchanathang@users.noreply.github.com>
2024-08-01 08:55:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,25 @@
-from flask import Flask, render_template, request, jsonify
-from meta_ai_api import MetaAI
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
-import logging
-from main import MetaAI
-import sys
-import os
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-src_dir = os.path.dirname(current_dir)
-sys.path.insert(0, src_dir)
-
+from meta_ai_api import MetaAI
+import os 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
-
+CORS(app) # <-- Enable CORS for all routes
 
 ai = MetaAI()
 
-logging.basicConfig(level=logging.DEBUG)
-
-
-# Serve the HTML file
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-# API endpoint
-@app.route('/endpoint', methods=['POST'])
+@app.route('/chatbot', methods=['POST'])
 def handle_chatbot_request():
     message = request.json['message']
     response = ai.prompt(message)
     return jsonify({'response': response})
 
-
 if __name__ == '__main__':
-#    app.run(debug=True)
-#if __name__ == '__main__':
-#  app.run(debug=True, host='0.0.0.0', port=5000)
+     app.run(debug=True)
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
